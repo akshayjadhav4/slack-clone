34de0188d06038b5a3a0f1c6991cd632320d5597
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-12 16:19:00 +0530

    header layout added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineOutlinedIcon from "@material-ui/icons/HelpOutlineOutlined";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar
+          className="header__avatar"
+          //   alt={user?.displayName}
+          //   src={user?.photoUrl}
+        />
+        <AccessTimeIcon />
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="header__right">
+        <HelpOutlineOutlinedIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
