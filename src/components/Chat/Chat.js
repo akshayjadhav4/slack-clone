fc6980ff38d0c7842f955732bf7b9598ce81dbb3
AKSHAY <akshayjadhav461999@gmail.com>
2020-08-13 17:29:20 +0530

    fetching and showing message from firestore

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,15 +4,24 @@ import { useParams } from "react-router-dom";
 import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import db from "../../config/firebase";
+import Message from "../Message/Message";
 function Chat() {
   const { roomId } = useParams();
   const [roomDetails, setRoomDetails] = useState(null);
+  const [roomMessages, setRoomMessages] = useState([]);
   useEffect(() => {
     if (roomId) {
       db.collection("rooms")
         .doc(roomId)
         .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
     }
+    db.collection("rooms")
+      .doc(roomId)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) =>
+        setRoomMessages(snapshot.docs.map((doc) => doc.data()))
+      );
   }, [roomId]);
 
   return (
@@ -29,6 +38,16 @@ function Chat() {
             <InfoOutlinedIcon /> Details
           </p>
         </div>
+      </div>
+      <div className="chat__messages">
+        {roomMessages.map(({ message, timestamp, user, userImage }, index) => (
+          <Message
+            message={message}
+            timestamp={timestamp}
+            user={user}
+            userImage={userImage}
+          />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/Message/Message.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Message.css";
+function Message({ message, timestamp, user, userImage }) {
+  return (
+    <div className="message">
+      <img src={userImage} alt={user} />
+      <div className="message__info">
+        <h4>
+          {user}{" "}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toLocaleString(undefined, {
+              timeZone: "Asia/Kolkata",
+            })}
+          </span>
+        </h4>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Message;
